918d141395b5e5fd3ed600ea89d3483af9b4d06e
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-05-16 11:54:15 +0200

    Replace Seq with GenSeq

--- src/main/scala/monte/pi/SamplerActor.scala
+++ src/main/scala/monte/pi/SamplerActor.scala
@@ -1,11 +1,12 @@
 package monte.pi
 
 import akka.actor.Actor
+import scala.collection.GenSeq
 
 object Sampler extends Types {
   val batchSize = 10000
   final case object GetBatch
-  final case class Batch(values: Seq[Point])
+  final case class Batch(values: GenSeq[Point])
 }
 
 class SamplerActor extends Actor with Types {
