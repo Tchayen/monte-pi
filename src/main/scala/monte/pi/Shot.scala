05feefee7891277422cb983cf2148f4361770260
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-06-11 12:40:18 +0200

    Fix bug in isAccurate (a => a * a)

--- src/main/scala/monte/pi/Shot.scala
+++ src/main/scala/monte/pi/Shot.scala
@@ -1,5 +1,5 @@
 package monte.pi
 
 class Shot(val x: Double, val y: Double) {
-  def isAccurate(a: Double): Boolean = (x * x) + (y * y) <= a
+  def isAccurate(a: Double): Boolean = (x * x) + (y * y) <= (a * a)
 }
