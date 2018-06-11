723caa36331c63fff6041d73bc32a467568ad80b
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-06-11 15:36:36 +0200

    Implement progressive processing

--- src/main/scala/monte/pi/Processor.scala
+++ src/main/scala/monte/pi/Processor.scala
@@ -1,7 +1,7 @@
 package monte.pi
 
 import akka.Done
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 
@@ -10,15 +10,41 @@ import scala.concurrent.Future
 object Processor extends App {
   implicit val system = ActorSystem("processor_system")
   implicit val materializer = ActorMaterializer()
-  implicit val size = 10
+  implicit val size = 1
 
   def runCount(n: Int): Future[Done] = Source.repeat(new Shooter(size))
     .take(n)
     .map(_.fire())
     .map(_.isAccurate(size))
     .fold(new Stats(0, 0))((acc, x) => acc.update(x))
-    .map(_.getPi())
+    .map(_.getPi)
     .runWith(Sink.foreach(println))
 
-  runCount(100000)
+  def runProgressive(n: Int) = {
+    // sent from actor to stream to "ack" processing of given element
+    val AckMessage = StatsReducerActor.Ack
+
+    // sent from stream to actor to indicate start, end or failure of stream:
+    val InitMessage = StatsReducerActor.StreamInitialized
+    val OnCompleteMessage = StatsReducerActor.StreamCompleted
+    val onErrorMessage = (ex: Throwable) ⇒ StatsReducerActor.StreamFailure(ex)
+
+    val receiver = system.actorOf(
+      Props(new StatsReducerActor()))
+    val sink = Sink.actorRefWithAck(
+      receiver,
+      onInitMessage = InitMessage,
+      ackMessage = AckMessage,
+      onCompleteMessage = OnCompleteMessage,
+      onFailureMessage = onErrorMessage
+    )
+
+    Source.repeat(new Shooter(size))
+      .take(n)
+      .map(_.fire())
+      .map(_.isAccurate(size))
+      .runWith(sink)
+  }
+
+  runProgressive(1000000)
 }
