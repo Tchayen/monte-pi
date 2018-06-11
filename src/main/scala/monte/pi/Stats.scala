307cceda82c9c89d7ebeb78486d62113153f6093
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-06-11 15:31:22 +0200

    Extend Stats functionality

--- src/main/scala/monte/pi/Stats.scala
+++ src/main/scala/monte/pi/Stats.scala
@@ -2,7 +2,12 @@ package monte.pi
 
 class Stats(val all: Int, val acc: Int) {
   def update(accurate: Boolean): Stats = new Stats(all + 1, if (accurate) acc + 1 else acc)
-  def getPi() : Double = 4.0 * acc.toDouble / all.toDouble;
 
-  override def toString = s"Stats(All shots: $all, Accurate ones $acc)"
+  def getPi: Double = 4.0 * acc.toDouble / all.toDouble
+
+  def getDelta: Double = Math.abs(getPi - realPi)
+
+  val realPi = 3.1415926535897932
+
+  override def toString = s"π = $getPi\tΔ = $getDelta\t($acc/$all)"
 }
