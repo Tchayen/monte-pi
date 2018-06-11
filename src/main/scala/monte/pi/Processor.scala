e1588d346936f8d508bd9ca299794e0ff25187aa
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-06-11 12:41:38 +0200

    Add runCount method to Processor object

--- src/main/scala/monte/pi/Processor.scala
+++ src/main/scala/monte/pi/Processor.scala
@@ -1,16 +1,24 @@
 package monte.pi
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+
+import scala.concurrent.Future
 
 object Processor extends App {
   implicit val system = ActorSystem("processor_system")
   implicit val materializer = ActorMaterializer()
   implicit val size = 10
 
-  val source = Source.repeat(new Shooter(size))
+  def runCount(n: Int): Future[Done] = Source.repeat(new Shooter(size))
+    .take(n)
     .map(_.fire())
     .map(_.isAccurate(size))
+    .fold(new Stats(0, 0))((acc, x) => acc.update(x))
+    .map(_.getPi())
     .runWith(Sink.foreach(println))
+
+  runCount(100000)
 }
